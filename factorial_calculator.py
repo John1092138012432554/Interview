8ba227212741ccc3551c52395d37c3a22475743e
John1092138012432554 <johnmichael113429@mcpsva.org>
2026-02-26 11:02:53 -0500

    Refresh
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,14 +1,22 @@
 """
 Filename: factorial_calculator.py
-Author: <NAME>
+Author: <Escobar, John>
 Created: <DATE>
 Instructor: Holtslander
 """
 
 def factorial():
-       cal = int(input("Enter a non-negative whole number on the line below:\n"))
-    for x in range(cal):
-        print(f"{x} * {x-x}")
+    cal = int(input("Enter a non-negative whole number on the line below:\n"))
+    print(f"{cal}!", end=" ")
+    for cal in range(cal, 0, -1):
+        if cal == 1:
+            print(cal, end=" ")
+        else:
+            print(cal, end=" * ")
+    print(f"= {cal * cal}", end=" \n")
+    if cal == "0":
+        print("0! = 1")
+
 
 # You should not need to change any code below this point
 def main():
@@ -20,5 +28,4 @@ def main():
     print("Thank you for using this factorial calculator!")
 
 if __name__ == "__main__":
-
     main()
